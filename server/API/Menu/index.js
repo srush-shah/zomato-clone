a859d996eaafc82e1a79624a4e7dc1612cd70960
Srushti <srushti1010shah@gmail.com>
2023-01-12 13:22:45 +0530

    Menu API completed

---
+++ server/API/Menu/index.js
@@ -0,0 +1,46 @@
+//Libraries
+import express from "express";
+import passport from "passport";
+
+//Database Model
+import { MenuModel, ImageModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+/*
+Route   /list - list of food items
+Desc    Get all list menu based on id
+Params  _id
+Access  Public
+Method  GET
+*/
+
+Router.get("/list/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const menu = await MenuModel.findOne(_id);
+
+    return res.json({ menu });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/*
+Route   /image
+Desc    Get all menu images based on id
+Params  _id
+Access  Public
+Method  GET
+*/
+
+Router.get("/image/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const menu = await ImageModel.findOne(_id);
+
+    return res.json({ menu });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
